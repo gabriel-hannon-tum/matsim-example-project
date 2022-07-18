bcf3b5080289fc882938d0bab6b690692d7bd0b3
gabriel-hannon-tum <92515557+gabriel-hannon-tum@users.noreply.github.com>
2022-07-18 09:07:47 +0200

    push

---
+++ src/main/java/emissions/CreateTransitVehicles.java
@@ -0,0 +1,26 @@
+package emissions;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.vehicles.*;
+
+public class CreateTransitVehicles {
+    public static void main(String[] args) {
+        Vehicles vehicles = VehicleUtils.createVehiclesContainer();
+        VehicleType type = VehicleUtils.createVehicleType(Id.create("ExpressBus", VehicleType.class));
+
+        type.setNetworkMode("bus");
+        VehicleCapacity capacity = type.getCapacity();
+        capacity.setSeats(30);
+        capacity.setStandingRoom(30);
+
+        vehicles.addVehicleType(type);
+
+        for(int i = 8*3600; i < 20*3600; i += 5 * 60){
+            Id<Vehicle> vehicleId = Id.createVehicleId("departure_" + i);
+            Vehicle vehicle = VehicleUtils.createVehicle(vehicleId, type);
+            vehicles.addVehicle(vehicle);
+        }
+
+        
+    }
+}
